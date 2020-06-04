195305e71c289d6cc96a01a4d5bf76cee5926255
kevindongzg <dongkevin94@gmail.com>
2020-06-04 23:00:21 +0800

    update

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
 export { parseData };
+
+function parseData(input) {
+    const output = new Array(input.data.length);
+    for (let i = 0; i < input.data.length; i++) {
+        output[i] = {
+            name: (input.data)[i][0],
+            age: (input.data)[i][1],
+            gender: (input.data)[i][2],
+            birthday: (input.data)[i][3]
+        };
+    }
+    return output;
+}
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,20 @@
-const inject
 export { inject };
+
+function inject(items, sections) {
+    const output = new Array(items.length + sections.length);
+
+    for (let i = 0; i < sections.length; i++) {
+        output[sections[i].index + i] = sections[i].content
+    }
+
+    for (let i = 0; i < items.length; i++) {
+        for (let j = 0; j < output.length; j++) {
+            if (output[j] == null) {
+                output[j] = items[i];
+                break;
+            }
+        }
+    }
+
+    return output;
+}
